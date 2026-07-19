968f2a0e891be21cc6827ddbe673c2678765cf10
Sheralima <sherofali@gmail.com>
2026-07-19 12:28:40 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,36 +3,49 @@
 // firebase.js
 // ==========================================
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
 
 // Firebase Configuration
+
 const firebaseConfig = {
 
-  apiKey: "YOUR_API_KEY",
+apiKey: "AIzaSyARX24b0EnCmh-3WcYfy9wtgj5vpHUNEPQ",
 
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+authDomain: "sherry-project-1214.firebaseapp.com",
 
-  projectId: "YOUR_PROJECT_ID",
+projectId: "sherry-project-1214",
 
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+storageBucket: "sherry-project-1214.firebasestorage.app",
 
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+messagingSenderId: "692003406873",
 
-  appId: "YOUR_APP_ID"
-npm install firebase
+appId: "1:692003406873:web:e38c4b3806d963b5761688",
+
+measurementId: "G-2RPT4PEXHX"
+
 };
 
+
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// Services
+
+// Authentication
+
 const auth = getAuth(app);
+
+
+// Database
 
 const db = getFirestore(app);
 
+
 // Export
+
 export { app, auth, db };
