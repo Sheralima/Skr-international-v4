93cbebe2b4461a4362bea6d9da27f7844bc119ba
Sheralima <sherofali@gmail.com>
2026-07-19 11:17:14 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,38 @@
+// ==========================================
+// SKR INTERNATIONAL V4
+// firebase.js
+// ==========================================
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+// Firebase Configuration
+const firebaseConfig = {
+
+  apiKey: "YOUR_API_KEY",
+
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+
+  projectId: "YOUR_PROJECT_ID",
+
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+
+  appId: "YOUR_APP_ID"
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Services
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+// Export
+export { app, auth, db };
